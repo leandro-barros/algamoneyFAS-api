f0171a85b82ed23b1cbeac17efc4fde5c6b5420f
Leandro Barros <leobarros.evangelista@gmail.com>
2019-02-01 20:41:43 -0200

    6.5. Configurando JWT no projeto.

--- src/main/java/com/algamoneyFAS/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/algamoneyFAS/api/config/AuthorizationServerConfig.java
@@ -9,35 +9,43 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-
+	
 	@Autowired
 	private AuthenticationManager authenticationManager;
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("angula").secret("@ngul@r0").scopes("read", "write") // Define cliente que pode
-																							// ter acesso somente a
-																							// leitura, pode define
-																							// scpes para diversos
-																							// clientes
-				.authorizedGrantTypes("password") // Autoriza o uses e senha para o cliente, nocaso o angula
-				.accessTokenValiditySeconds(1800); // duração do token
+		clients.inMemory()
+			.withClient("angular")
+			.secret("@ngul@r0")
+			.scopes("read","write") // Define cliente que pode ter acesso somente a leitura, pode define scpes para diversos clientes
+			.authorizedGrantTypes("password") // Autoriza o uses e senha para o cliente, nocaso o angula
+			.accessTokenValiditySeconds(1800); // duração do token
 	}
-
+	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
-
+		endpoints
+			.tokenStore(tokenStore())
+			.accessTokenConverter(accessTokenConverter())
+			.authenticationManager(authenticationManager);
 	}
-
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("chave");
+		return accessTokenConverter;
+	}
 	// Armazena os tokens
 	@Bean
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		return new JwtTokenStore(accessTokenConverter());
 	}
 }
