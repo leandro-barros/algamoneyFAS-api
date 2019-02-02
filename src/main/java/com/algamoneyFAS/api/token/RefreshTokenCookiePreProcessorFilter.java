3e2108d1ff656793fdd8d3f11dff7c6dfbc4ea11
Leandro Barros <leobarros.evangelista@gmail.com>
2019-02-02 18:21:13 -0200

    6.8. Movendo o refresh token do cookie para a requisição.

---
+++ src/main/java/com/algamoneyFAS/api/token/RefreshTokenCookiePreProcessorFilter.java
@@ -0,0 +1,71 @@
+package com.algamoneyFAS.api.token;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+import org.apache.catalina.util.ParameterMap;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RefreshTokenCookiePreProcessorFilter implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+		
+		if ("/oauth/token".equalsIgnoreCase(httpServletRequest.getRequestURI())
+				&& "refresh_token".equalsIgnoreCase(httpServletRequest.getParameter("grant_type")) // classe authorization server
+				&& httpServletRequest.getCookies() != null) {
+			for (Cookie cookie : httpServletRequest.getCookies()) {
+				if (cookie.getName().equals("refreshToken")) {
+					String refreshToken = cookie.getValue();
+					httpServletRequest = new MyServletRequestWrapper(httpServletRequest, refreshToken);
+				}
+			}
+		}
+		chain.doFilter(httpServletRequest, response);
+	}
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+	
+	@Override
+	public void destroy() {
+	}
+	
+	//Classe Sobrecresve outro objeto(HttpServletRequestWrapper) que tenha a requisição, 
+	//pq a requisão depois pronto não pode ser alterado  e assim como Mapa
+	static  class MyServletRequestWrapper extends HttpServletRequestWrapper{
+
+		private String refreshToken;
+		
+		public MyServletRequestWrapper(HttpServletRequest request, String refreshToken) {
+			super(request);
+			this.refreshToken = refreshToken;
+		}
+		
+		// parametro da requisição para alterar a requisição
+		@Override
+		public Map<String, String[]> getParameterMap() {
+			ParameterMap<String, String[]> map = new ParameterMap<>(getRequest().getParameterMap());
+			map.put("refresh_token", new String[] {refreshToken});
+			map.setLocked(true);
+			return map;
+		}
+	}
+}
