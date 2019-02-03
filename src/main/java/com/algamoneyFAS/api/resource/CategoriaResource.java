8df3c0bc3a7cc96cd62ea788d13613bf82cdb847
Leandro Barros <leobarros.evangelista@gmail.com>
2019-02-03 16:59:33 -0200

    6.9. O que é CORS.

--- src/main/java/com/algamoneyFAS/api/resource/CategoriaResource.java
+++ src/main/java/com/algamoneyFAS/api/resource/CategoriaResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,8 @@ public class CategoriaResource {
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
+//	// Somente esta url pode fazer requisão get para a api
+//	@CrossOrigin(maxAge = 10, origins = { "http://localhost:8000" })
 	@GetMapping
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
