31d89cb08efd355bd06a3aa85b8c6ef0100fa165
Leandro Barros <leobarros.evangelista@gmail.com>
2019-02-21 07:38:09 -0300

    7.7. Desafio - Pesquisa de pessoa.

--- src/main/java/com/algamoneyFAS/api/repository/PessoaRepository.java
+++ src/main/java/com/algamoneyFAS/api/repository/PessoaRepository.java
@@ -1,19 +1,14 @@
 package com.algamoneyFAS.api.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import com.algamoneyFAS.api.model.Pessoa;
-//
-//public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
-//
-//}
 
 
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algamoneyFAS.api.model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+	public Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
 
 }
--- src/main/java/com/algamoneyFAS/api/resource/PessoaResource.java
+++ src/main/java/com/algamoneyFAS/api/resource/PessoaResource.java
@@ -1,12 +1,12 @@
 package com.algamoneyFAS.api.resource;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,10 +40,16 @@ public class PessoaResource {
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
+//	@GetMapping
+//	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
+//	public List<Pessoa> listar(){
+//		return pessoaRepository.findAll();
+//	}
+	
 	@GetMapping
-	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
-	public List<Pessoa> listar(){
-		return pessoaRepository.findAll();
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA')")
+	public Page<Pessoa> pesquisar(@RequestParam(required = false, defaultValue = "%") String nome, Pageable pageable) {
+		return pessoaRepository.findByNomeContaining(nome, pageable);
 	}
 
 	@PostMapping
