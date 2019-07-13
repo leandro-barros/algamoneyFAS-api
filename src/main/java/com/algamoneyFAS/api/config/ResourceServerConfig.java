4910cd0e45c8dc987b98cdad425faeebc0fac4fb
Leandro Barros <leobarros.evangelista@gmail.com>
2019-07-13 20:34:13 -0300

    Deploy

--- src/main/java/com/algamoneyFAS/api/config/ResourceServerConfig.java
+++ src/main/java/com/algamoneyFAS/api/config/ResourceServerConfig.java
@@ -18,7 +18,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
-@Profile("oauth-security")
+//@Profile("oauth-security")
 @Configuration
 @EnableWebSecurity
 @EnableResourceServer
